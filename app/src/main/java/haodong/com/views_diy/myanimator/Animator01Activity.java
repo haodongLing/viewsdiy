822fbc7619c3ad6b5e546f05b517f0b84a6d822f
linghailong <linghailong@amberweather.com>
2019-01-22 17:45:02 +0800

    some

---
+++ app/src/main/java/haodong/com/views_diy/myanimator/Animator01Activity.java
@@ -0,0 +1,20 @@
+package haodong.com.views_diy.myanimator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import haodong.com.views_diy.R;
+
+/**
+ * @Auther linghailong
+ * created at 2018/12/24
+ * @duscribe:
+ */
+public class Animator01Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animator01);
+    }
+}
--- app/src/main/java/haodong/com/views_diy/myg11n27/MyPlayer.java
+++ app/src/main/java/haodong/com/views_diy/myg11n27/MyPlayer.java
@@ -1,6 +1,12 @@
 package haodong.com.views_diy.myg11n27;
 
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.view.SurfaceHolder;
+
+import java.io.IOException;
 
 /**
  * @author linghailong
@@ -8,13 +14,14 @@ import android.media.MediaPlayer;
  * @email 105354999@qq.com
  * @describe :
  */
-public class MyPlayer implements MediaPlayer.OnPreparedListener,MediaPlayer.OnCompletionListener,
+public class MyPlayer implements MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener,
         MediaPlayer.OnErrorListener {
     private MediaPlayer mPlayer;
     private boolean hasPrepared;
-    private void initIfNessary(){
-        if (null==mPlayer){
-            mPlayer=new MediaPlayer();
+
+    private void initIfNessary() {
+        if (null == mPlayer) {
+            mPlayer = new MediaPlayer();
             mPlayer.setOnErrorListener(this);
             mPlayer.setOnCompletionListener(this);
             mPlayer.setOnPreparedListener(this);
@@ -23,8 +30,7 @@ public class MyPlayer implements MediaPlayer.OnPreparedListener,MediaPlayer.OnCo
 
     @Override
     public void onCompletion(MediaPlayer mp) {
-        hasPrepared=false;
-
+        hasPrepared = false;
     }
 
     @Override
@@ -32,13 +38,61 @@ public class MyPlayer implements MediaPlayer.OnPreparedListener,MediaPlayer.OnCo
         return false;
     }
 
-    /**
-     * 准备完成
-     * @param mp
-     */
     @Override
     public void onPrepared(MediaPlayer mp) {
-        hasPrepared=true;
+        hasPrepared = true;
+    }
 
+    public void play(Context context, String dataSource) {
+        hasPrepared = false;
+        initIfNessary();
+        //设置音频流类型
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        // 设置播放的视频源
+        AssetFileDescriptor fd = null;
+        try {
+            fd = context.getAssets().openFd(dataSource);
+            mPlayer.setDataSource(fd.getFileDescriptor(), fd.getStartOffset(),
+                    fd.getLength());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mPlayer.prepareAsync();
+    }
+    public void start(){
+        if (null!=mPlayer&&hasPrepared){
+            mPlayer.start();
+        }
+    }
+    public void pause(){
+        if (null!=mPlayer&&hasPrepared){
+            mPlayer.pause();
+        }
+    }
+
+    public void seekTo(int position){
+        if(null!=mPlayer&&hasPrepared){
+            mPlayer.seekTo(position);
+        }
+    }
+
+    /**
+     * 绑定surfaceHolder
+     * @param holder
+     */
+    public void setDisplay(SurfaceHolder holder){
+        if (null!=mPlayer){
+            mPlayer.setDisplay(holder);
+        }
+    }
+
+    /**
+     * 释放资源
+     */
+    public void release(){
+        hasPrepared=false;
+        mPlayer.stop();
+        mPlayer.release();
+        mPlayer=null;
     }
 }
