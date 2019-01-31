4f7c55afb5ccc2fd7c017ea1371ad0407db95e85
linghailong <linghailong@amberweather.com>
2019-01-31 20:55:22 +0800

    LetterSidebar completed i

--- viewday1/src/main/java/com/example/haodong/viewday1/day7/LetterSideBar.java
+++ viewday1/src/main/java/com/example/haodong/viewday1/day7/LetterSideBar.java
@@ -1,12 +1,10 @@
 package com.example.haodong.viewday1.day7;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
