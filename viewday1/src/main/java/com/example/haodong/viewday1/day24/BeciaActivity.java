5fe26ea67ad632e06a1bf97c814c83bd925ce6da
linghaodong <1052354999@qq.com>
2019-02-22 01:08:03 +0800

    贝塞尔开端

---
+++ viewday1/src/main/java/com/example/haodong/viewday1/day24/BeciaActivity.java
@@ -0,0 +1,15 @@
+package com.example.haodong.viewday1.day24;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.haodong.viewday1.R;
+
+public class BeciaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_becia);
+    }
+}
---
+++ viewday1/src/main/java/com/example/haodong/viewday1/day24/MessageBubbleView.java
@@ -0,0 +1,103 @@
+package com.example.haodong.viewday1.day24;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.example.haodong.viewday1.SizeUtils;
+
+/**
+ * package_name: com.example.haodong.viewday1.day24
+ * description: 1. 不动圆的半径是变化的。有一个初始大小，而且小到一定大小后回消失
+ * author: linghaoDo
+ * date: 2019/2/22
+ */
+public class MessageBubbleView extends View {
+    /*两个圆的圆心*/
+    private PointF mFixactionPoint, mDragPoint;
+    /*圆的半径*/
+    private int mDragCircleRandius = SizeUtils.dip2px(getContext(), 10),
+            mFixedCircleRandius = SizeUtils.dip2px(getContext(), 10);
+    /*画圆的画笔*/
+    private Paint mDragPaint, mFixedPaint;
+
+
+    public MessageBubbleView(Context context) {
+        this(context, null);
+    }
+
+    public MessageBubbleView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MessageBubbleView(Context context, AttributeSet attrs,
+                             int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mFixedPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mFixedPaint.setStyle(Paint.Style.FILL);
+        mFixedPaint.setColor(Color.RED);
+        /*防抖动*/
+        mFixedPaint.setDither(true);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                /*手指按下 指定当前的位置*/
+                float downX = event.getX();
+                float downY = event.getY();
+                initPoint(downX, downY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float currentX = event.getX();
+                float currentY = event.getY();
+                /*计算两个点的距离*/
+                updateDragPoint(currentX, currentY);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+
+        }
+        invalidate();
+        return true;
+    }
+
+    /**
+     * 更新拖拽点的位置
+     *
+     * @param currentX
+     * @param currentY
+     */
+    private void updateDragPoint(float currentX, float currentY) {
+        mDragPoint.x = currentX;
+        mDragPoint.y = currentY;
+    }
+
+    /**
+     * 初始化位置
+     *
+     * @param downX
+     * @param downY
+     */
+    private void initPoint(float downX, float downY) {
+        mFixactionPoint = new PointF(downX, downY);
+        mDragPoint = new PointF(downX, downY);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        if (mDragPoint == null || mFixactionPoint == null) {
+            return;
+        }
+        /*画两个圆*/
+        // 画拖拽圆
+        canvas.drawCircle(mDragPoint.x, mDragPoint.y, );
+    }
+}
