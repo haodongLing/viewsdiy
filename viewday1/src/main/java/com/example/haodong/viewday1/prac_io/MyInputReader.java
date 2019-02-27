ecee461e80ca8de9535637c8c4b01efa72d958a7
linghaodong <1052354999@qq.com>
2019-02-28 00:42:59 +0800

    io_prac

---
+++ viewday1/src/main/java/com/example/haodong/viewday1/prac_io/MyInputReader.java
@@ -0,0 +1,30 @@
+package com.example.haodong.viewday1.prac_io;
+
+import java.io.Closeable;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * package_name: com.example.haodong.viewday1.prac_io
+ * description:
+ * author: linghaoDo
+ * date: 2019/2/28
+ */
+public class MyInputReader {
+    private static final int BUFFER_SIZE = 1024;
+
+    public static void main(String[] args) {
+        FileReader fr = null;
+        FileWriter fw = null;
+    }
+
+    public static class CloseUtils {
+        public static void setClose(T<? extends Closeable> t) {
+
+        }
+    }
+
+}
