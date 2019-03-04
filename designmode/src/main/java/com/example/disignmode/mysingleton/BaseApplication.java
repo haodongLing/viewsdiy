be53753f2dbab640342f49d71469801bec1f9bc8
linghaodong <1052354999@qq.com>
2019-03-05 01:09:01 +0800

    singleton

---
+++ designmode/src/main/java/com/example/disignmode/mysingleton/BaseApplication.java
@@ -0,0 +1,54 @@
+package com.example.disignmode.mysingleton;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+
+import java.io.PipedWriter;
+
+/**
+ * Created by hcDarren on 2017/9/17.
+ */
+
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
+    }
+}
